3f68c86e0a33ddc09616e6469406993c7abc262f
suryaa-004 <suryaarul2001@gmail.com>
2024-02-09 19:13:39 +0530

    first commit

---
+++ employee-management-system/src/test/java/com/java/employee/management/system/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.employee.management.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
